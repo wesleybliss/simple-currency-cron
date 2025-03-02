9879443d1ba29bbcf803c34f990d3eaf80a4e613
Wesley Bliss <wesley.bliss@gmail.com>
2025-03-02 18:51:33 -0500

    Cleanup

--- app/page.jsx
+++ app/page.jsx
@@ -1,16 +1,12 @@
 
-const Home = () => {
+export default function Home() {
     
     return (
         
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            
             <h4>@todo</h4>
-            
         </main>
         
     )
     
 }
-
-export default Home
